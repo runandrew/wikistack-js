c5486f08f103eac196d9bede54229f49da47bb1b
Andrew Garcia <runandrew@gmail.com>
2016-11-14 17:03:55 -0500

    Adding users functionality to app

--- routes/wiki.js
+++ routes/wiki.js
@@ -7,48 +7,60 @@ let Page = models.Page;
 let User = models.User;
 
 Router.get('/', function(req, res, next) {
-  Page.findAll()
+    Page.findAll()
     .then(function(foundPages){
-      res.render('index', {
-        pages:foundPages
-      });
+        res.render('index', {
+            pages:foundPages
+        });
     })
-    .catch(next);});
+    .catch(next);
+});
 
 Router.post('/', function(req, res, next) {
 
     var page = Page.build({
-      title: req.body.title,
-    //   urlTitle: ,
-      content: req.body.content,
-      status: req.body.status
+        title: req.body.title,
+        content: req.body.content,
+        status: req.body.status
     });
 
 
+    User.findOrCreate({where: {email: req.body.email, name: req.body.name}}).spread(function(user, boolean) {
+        
+    }); // [user, boolean]
+
+    // .then((userArr) => {
+    //     let user = userArr[0];
+    // });
+    //
+    // .spread((user, wasCreated) => {
+    //     res.send(user);
+    // })
+
 
     page.save().then(function(savedPage){
-     res.redirect(savedPage.route); // route virtual FTW
+        res.redirect(savedPage.route); // route virtual FTW
     }).catch(next);
 });
 
 Router.get('/add', function(req, res, next) {
-  res.render('addpage')
+    res.render('addpage')
 });
 
 Router.get("/:urlTitle", function(req, res, next) {
-    
-  Page.findOne({ 
-    where: { 
-      urlTitle: req.params.urlTitle 
-    } 
-  })
-  .then(function(foundPage){
-    res.render('wikipage', {
-      title: foundPage.title,
-      content: foundPage.content
-    });
-  })
-  .catch(next);
+
+    Page.findOne({
+        where: {
+            urlTitle: req.params.urlTitle
+        }
+    })
+    .then(function(foundPage){
+        res.render('wikipage', {
+            title: foundPage.title,
+            content: foundPage.content
+        });
+    })
+    .catch(next);
 
 
 });
